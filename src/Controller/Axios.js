dd3e94a0e0582f4d9b3116dc4d014f59c5a498ec
Sanskar <sanskarkumar85111@gmail.com>
2022-02-25 02:02:53 +0530

    final commmit

--- src/Controller/Axios.js
+++ src/Controller/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:9000';
+const baseURL = process.env.REACT_APP_BASE_URL || 'https://us-inventory.herokuapp.com';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
